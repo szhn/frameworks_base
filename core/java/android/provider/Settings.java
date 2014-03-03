9b32b34362b5637e07c3002396215d70e3906e1f
GwonHyeok <kh4975@naver.com>
2014-03-03 11:43:07 +0000

    FW_Base - XuiMod: Toast Animations [1/2]
    @neobuddy89: Moved to Global. Updated for Android 10.
    
    From: westcripp <altan_koruk@hotmail.com>
    Date: Fri, 23 Dec 2016 22:45:27 +0300
    Subject: [PATCH] Frameworks: Re-adapt toast window animations for 7.1.1
    
     Since google updated toast popup notifications on 7.1.1 release. toast window
     animations stop working. So now this is make it work again
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14784,6 +14784,12 @@ public final class Settings {
         public static final String ADB_PORT = "adb_port";
 
         /**
+         * Toast animations
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
