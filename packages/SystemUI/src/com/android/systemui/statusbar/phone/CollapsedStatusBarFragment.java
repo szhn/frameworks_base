ea1bb79b2e6651847387a4c751061d7e3ce286c9
Brett Rogers <brettrogers11@gmail.com>
2017-10-16 16:32:24 +0200

    base: Status bar notification ticker [1/2]
    aicp SpiritCroc: temporarily remove music ticker and port to Pie
    abc ezio84: cleanup and port to Oreo
    du nychitman1: cleanup and port to Nougat
    
    Squashed:
    
    	Make status bar ticker play nice with center clock
    
    	Ticker: Clear ticker regardless of setting
    
    	Fix infinite ticker when showing while getting disabled
    
    	Stop ticker on theme changes
    
    	Configurable status bar ticker duration [1/2]
    
    	Media tickers: fallback to notification text if no metadata available
    
    	Return of the ticker scroll [1/2]
    
    	Music ticker: once per track is enough
    
    	Remember the track that last was ticker-ed, and only ticker again
    	if actually something changed, instead of spamming with tickers
    	so that the status bar gets unusable.
    
    	Ticker: fix SystemUI FCs and adapt code to aosp statusbar fragment cycle
    
    	e.g.: change systemui theme a couple of times. or set dui navbar height to higher value then smaller
    	then go to home, SysUI will FC.
    
    	also improve views refreshing on configuration changes (e.g. screen
    	rotation) to avoid empty statusbar
    
    	Ticker: fix empty views on configuration changes
    
    	to replicate: toggle navbar off and on again, tick something
    
    	Notification ticker: fix FC
    
    	Statusbar ticker refactoring
    
    	cleanup-fixes for O
    	apply darkintensity on white statusbar
    	add music ticker mode to show also music track info on new track being played
    	don't tick on heads up
    	use a gentle smooth fade animation instead of scrolling up/down statusbar
    
    	Status bar notification ticker
    
    	abc ezio84: cleanup and port to Oreo
    	du nychitman1: cleanup and port to Nougat

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -32,6 +32,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
+import android.widget.ImageSwitcher;
 import android.widget.LinearLayout;
 
 import com.android.systemui.Dependency;
@@ -43,6 +44,7 @@ import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.StatusBarState;
 import com.android.systemui.statusbar.phone.StatusBarIconController.DarkIconManager;
 import com.android.systemui.statusbar.policy.Clock;
+import com.android.systemui.statusbar.phone.TickerView;
 import com.android.systemui.statusbar.policy.EncryptionHelper;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
 import com.android.systemui.statusbar.policy.NetworkController;
@@ -110,6 +112,10 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
        }
     }
     private SettingsObserver mSettingsObserver = new SettingsObserver(mHandler);
+
+    // Notification ticker
+    private int mTickerEnabled;
+    private View mTickerViewFromStub;
 
     private SignalCallback mSignalCallback = new SignalCallback() {
         @Override
@@ -463,8 +469,11 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         }
         mShowCarrierLabel = Settings.System.getIntForUser(mContentResolver,
                 Settings.System.STATUS_BAR_SHOW_CARRIER, 0, UserHandle.USER_CURRENT);
+        mTickerEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_TICKER, 0, UserHandle.USER_CURRENT);
         updateClockStyle(animate);
         setCarrierLabel(animate);
+        initTickerView();
     }
 
     private void updateClockStyle(boolean animate) {
@@ -482,6 +491,21 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             animateShow(mCustomCarrierLabel, animate);
         } else {
             animateHide(mCustomCarrierLabel, animate, false);
+        }
+    }
+
+    private void initTickerView() {
+        if (mTickerEnabled != 0) {
+            View tickerStub = mStatusBar.findViewById(R.id.ticker_stub);
+            if (mTickerViewFromStub == null && tickerStub != null) {
+                mTickerViewFromStub = ((ViewStub) tickerStub).inflate();
+            }
+            TickerView tickerView = (TickerView) mStatusBar.findViewById(R.id.tickerText);
+            ImageSwitcher tickerIcon = (ImageSwitcher) mStatusBar.findViewById(R.id.tickerIcon);
+            mStatusBarComponent.createTicker(
+                    mTickerEnabled, getContext(), mStatusBar, tickerView, tickerIcon, mTickerViewFromStub);
+        } else {
+            mStatusBarComponent.disableTicker();
         }
     }
 }
