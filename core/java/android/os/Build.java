91858e8513fc68676c2b522a84e727b39e221dc9
maxwen <max.weninger@gmail.com>
2018-09-14 17:34:30 -0500

    Store Titanium fingerprint for package data
    and use that to clear package cache instead of our
    recovery script solution
    
    Change-Id: I21f599a84f8ff28b53bc088f739374c037f81be0

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -1002,6 +1002,19 @@ public class Build {
     public static final String FINGERPRINT = deriveFingerprint();
 
     /**
+     * Evolution X fingerprint
+     * @hide
+     */
+    public static final String PROP_TITANIUM_FINGERPRINT = "org.titanium.fingerprint";
+
+    /**
+     * Evolution X fingerprint
+     * @hide
+     */
+    public static final String TITANIUM_FINGERPRINT = SystemProperties.get(PROP_TITANIUM_FINGERPRINT,
+            deriveFingerprint());
+
+    /**
      * Some devices split the fingerprint components between multiple
      * partitions, so we might derive the fingerprint at runtime.
      */
