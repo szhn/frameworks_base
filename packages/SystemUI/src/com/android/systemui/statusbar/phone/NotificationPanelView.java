f0b1e7b679dea0ae430a7185510d0f14128a757b
maxwen <max.weninger@gmail.com>
2019-11-18 00:29:15 +0100

    SystemUI: use DOUBLE_TAP_TO_WAKE setting also for wake from aod
    Change-Id: I8ca091471e78bf1624935c7ea7444e93fc89a8ce

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1286,7 +1286,7 @@ public class NotificationPanelView extends PanelView implements
         // pull down QS or expand the shade.
         if (mStatusBar.isBouncerShowingScrimmed()) {
             return false;
-	}
+        }
         if (!mQsExpanded
                 && mDoubleTapToSleepEnabled
                 && event.getY() < mStatusBarHeaderHeight) {
