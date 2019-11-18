cdc25243456e49c8c86d76e7c2cf2f34c56804f6
Nicolas Dhouailly <ndhouailly@gmail.com>
2019-11-18 21:37:39 +0100

    Allow to hide arrow for back gesture [1/2]
    @SKULSHADY: Reverse the logic of the toggle
    Change-Id: Ia1465c7ce47ffa6da719e40140857b94646ac402
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4184,6 +4184,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.USE_OLD_MOBILETYPE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.SHOW_BACK_ARROW_GESTURE),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4228,6 +4231,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateChargingAnimation();
             setGamingMode();
             setOldMobileType();
+            setHideArrowForBackGesture();
         }
     }
 
@@ -4319,6 +4323,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                 Settings.System.GAMING_MODE_HEADSUP_TOGGLE, 1,
                 UserHandle.USER_CURRENT) == 1;
         mNotificationInterruptionStateProvider.setGamingPeekMode(mGamingModeActivated && mHeadsUpDisabled);
+    }
+
+    private void setHideArrowForBackGesture() {
+        if (getNavigationBarView() != null) {
+            getNavigationBarView().updateBackArrowForGesture();
+        }
     }
 
     public int getWakefulnessState() {
