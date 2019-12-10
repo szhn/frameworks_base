ccfd4fffd1ef77255ec04706984861d21eef3657
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-10 13:28:16 +0000

    Revert latest fod related commits
    Revert "FOD: Enable burn-in protection only while dreaming"
    
    This reverts commit 6419a35d98b97937f0cbfb480c8928f4916a0734.
    
    Revert "FOD: let displaymanager handle brightness"
    
    This reverts commit cda23b5dd7189f1209f509b5720a7fb365c415b6.
    
    Revert "FOD: change a few things about FOD behaviour"
    
    This reverts commit 3eb1e3d1bf780ab30f6d4b1a118d0b1130e3a3c1.
    
    Revert "SystemUI: add pulsing callback to KeyguardUpdateMonitor"
    
    This reverts commit be8615c608fd19a166e29d1003e02f84ec2548de.
    
    Change-Id: I2a7aad8375411299ab6e685db695a9244a1fc598

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4595,7 +4595,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                     callback.onPulseStarted();
                     updateNotificationPanelTouchState();
                     setPulsing(true);
-                    KeyguardUpdateMonitor.getInstance(mContext).setPulsing(true);
                 }
 
                 @Override
@@ -4608,7 +4607,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                         mStatusBarWindow.suppressWakeUpGesture(false);
                     }
                     setPulsing(false);
-                    KeyguardUpdateMonitor.getInstance(mContext).setPulsing(false);
                 }
 
                 private void setPulsing(boolean pulsing) {
