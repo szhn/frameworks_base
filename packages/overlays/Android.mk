1c14cc003a4a8c65512702ae953ce0db1b1ba3cc
AnkitGourav <Ankitgourav035@gmail.com>
2020-01-21 14:55:41 +0530

    overlays: Remove AOSP Accents [1/2]

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -17,25 +17,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := frameworks-base-overlays
 LOCAL_REQUIRED_MODULES := \
-	AccentColorBlackOverlay \
-	AccentColorCinnamonOverlay \
-	AccentColorOceanOverlay \
-	AccentColorOrchidOverlay \
-	AccentColorSpaceOverlay \
-	AccentColorGreenOverlay \
-	AccentColorPurpleOverlay \
-	AccentColorCocaColaOverlay \
-	AccentColorDiscordOverlay \
-	AccentColorGoldenShowerOverlay \
-	AccentColorJollibeeOverlay \
-	AccentColorNextbitOverlay \
-	AccentColorOnePlusOverlay \
-	AccentColorParanoidOverlay \
-	AccentColorPepsiOverlay \
-	AccentColorRazerOverlay \
-	AccentColorStarbucksOverlay \
-	AccentColorXboxOverlay \
-	AccentColorXiaomiOverlay \
 	DisplayCutoutEmulationCornerOverlay \
 	DisplayCutoutEmulationDoubleOverlay \
 	DisplayCutoutEmulationTallOverlay \
