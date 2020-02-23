703636ccad891cb902ff415d18d52d11affe0bad
Joey Huab <joey@evolution-x.org>
2020-02-23 06:34:17 +0000

    SystemUI: Add new FOD pressed state icons [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5821,7 +5821,7 @@ public final class Settings {
 
         /** @hide */
         private static final Validator FOD_PRESSED_STATE_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 2);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 6);
 
         /**
          * FOD recognizing animation picker
