3a8a84b7004c88b5a394fb367de835691beba43f
Pranav Vashi <neobuddy89@gmail.com>
2020-03-05 21:54:57 +0530

    PowerManagerService: Improve waking device when prox check disabled

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1206,15 +1206,12 @@ public final class PowerManagerService extends SystemService
         mButtonLightOnKeypressOnly = Settings.System.getIntForUser(resolver,
                 Settings.System.BUTTON_BACKLIGHT_ONLY_WHEN_PRESSED,
                 0, UserHandle.USER_CURRENT) == 1;
-
-        mProximityWakeEnabled = Settings.System.getInt(resolver,
-                Settings.System.PROXIMITY_ON_WAKE,
-                mProximityWakeEnabledByDefaultConfig ? 1 : 0) == 1;
-
         mHardwareKeysDisable = Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
                 UserHandle.USER_CURRENT) != 0;
-
+        mProximityWakeEnabled = Settings.System.getIntForUser(resolver,
+                Settings.System.PROXIMITY_ON_WAKE,
+                mProximityWakeEnabledByDefaultConfig ? 1 : 0, UserHandle.USER_CURRENT) == 1;
         mWakeUpWhenPluggedOrUnpluggedSetting = Settings.Global.getInt(resolver,
                 Settings.Global.WAKE_WHEN_PLUGGED_OR_UNPLUGGED,
                 (mWakeUpWhenPluggedOrUnpluggedConfig ? 1 : 0));
@@ -4778,8 +4775,17 @@ public final class PowerManagerService extends SystemService
                     }
                 }
             };
-            if (checkProximity) {
-                runWithProximityCheck(r);
+            if (checkProximity && mProximityWakeSupported && mProximityWakeEnabled
+                    && mProximitySensor != null) {
+
+                final TelephonyManager tm = mContext.getSystemService(TelephonyManager.class);
+                final boolean hasIncomingCall = tm.getCallState() == TelephonyManager.CALL_STATE_RINGING;
+
+                if (!hasIncomingCall) {
+                    runWithProximityCheck(r);
+                } else {
+                    r.run();
+                }
             } else {
                 r.run();
             }
@@ -5457,19 +5463,10 @@ public final class PowerManagerService extends SystemService
             return;
         }
 
-        final TelephonyManager tm =
-                (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        final boolean hasIncomingCall = tm.getCallState() == TelephonyManager.CALL_STATE_RINGING;
-
-        if (mProximityWakeSupported && mProximityWakeEnabled
-                && mProximitySensor != null && !hasIncomingCall) {
-            final Message msg = mHandler.obtainMessage(MSG_WAKE_UP);
-            msg.obj = r;
-            mHandler.sendMessageDelayed(msg, mProximityTimeOut);
-            runPostProximityCheck(r);
-        } else {
-            r.run();
-        }
+        final Message msg = mHandler.obtainMessage(MSG_WAKE_UP);
+        msg.obj = r;
+        mHandler.sendMessageDelayed(msg, mProximityTimeOut);
+        runPostProximityCheck(r);
     }
 
     private void runPostProximityCheck(final Runnable r) {
