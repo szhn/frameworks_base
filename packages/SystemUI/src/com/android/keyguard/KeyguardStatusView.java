26b039fe97bb1a5ed7520b390574c0aab95b1fdf
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-23 21:19:13 -0300

    SystemUI: Conditionally hide extra biometric help on keyguard

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -26,6 +26,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.Html;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
