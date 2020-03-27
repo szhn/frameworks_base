ebbe3a5f41b4e2828d6cad098765ae8756a22d62
RealSchnedi <schnedi3@gmail.com>
2020-03-28 00:26:07 +0100

    Fix path

--- packages/SystemUI/src/com/android/systemui/omni/FileHeaderProvider.java
+++ packages/SystemUI/src/com/android/systemui/omni/FileHeaderProvider.java
@@ -37,7 +37,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
 
-import com.android.internal.util.evolution.ImageHelper;
+import com.android.internal.util.titanium.ImageHelper;
 import com.android.systemui.R;
 
 public class FileHeaderProvider implements
