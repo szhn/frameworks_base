ded77ccc2b32c924c66eaf490ae57008f9db23d7
RealSchnedi <schnedi3@gmail.com>
2020-03-29 23:04:35 +0200

    Fix previous commit

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -193,7 +193,7 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.EVO_SETTINGS;
+        return MetricsEvent.TIELEMENTS;
     }
 
     @Override
