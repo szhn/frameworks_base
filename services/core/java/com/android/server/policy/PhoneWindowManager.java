AOSiP/platform_frameworks_base

base:PhoneWindowManager: simplify toggleCameraFlash
Change-Id: I8b43e6f480ae8d09cf302f9695168cc20e5ffe42

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -802,7 +802,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mMovingDisplayToTopKeyTriggered = false;
                     break;
                 case MSG_TOGGLE_TORCH:
-                    toggleFlashLight();
+                    aosipUtils.toggleCameraFlash();
                     break;
                 case MSG_DISPATCH_VOLKEY_SKIP_TRACK: {
                     sendSkipTrackEventToStatusBar(msg.arg1);
@@ -811,11 +811,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
             }
         }
-    }
-
-    private void toggleFlashLight() {
-        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Flashlight toggle");
-        aosipUtils.toggleCameraFlash();
     }
 
     private UEventObserver mHDMIObserver = new UEventObserver() {
@@ -1320,7 +1315,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         switch (behavior) {
             case MULTI_PRESS_POWER_NOTHING:
                 if ((mTorchActionMode == 1) && (!isScreenOn() || isDozeMode())) {
-                    toggleFlashLight();
+                    aosipUtils.toggleCameraFlash();
                 }
                 break;
             case MULTI_PRESS_POWER_THEATER_MODE:

szhn/frameworks_base

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -855,15 +855,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     break;
                 }
                 case MSG_TOGGLE_TORCH:
-                    toggleFlashLight();
+                    TitaniumUtils.toggleCameraFlash();
                     break;
             }
         }
-    }
-
-    private void toggleFlashLight() {
-        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Flashlight toggle");
-        EvolutionUtils.toggleCameraFlash();
     }
 
     private UEventObserver mHDMIObserver = new UEventObserver() {
@@ -1391,7 +1386,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         switch (behavior) {
                 case MULTI_PRESS_POWER_NOTHING:
                 if ((!isScreenOn() || isDozeMode())) {
-                    toggleFlashLight();
+                    TitaniumUtils.toggleCameraFlash();
                 }
                 break;
             case MULTI_PRESS_POWER_THEATER_MODE: