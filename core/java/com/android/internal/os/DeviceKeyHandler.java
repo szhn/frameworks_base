920ec9432fc29af70e4f1acabfc6d0200dfee54c
Mazda-- <du.alexcruz@gmail.com>
2020-04-10 06:04:52 +0200

    Screenrecord dialog: Make it less ugly
    - Fixed RTL fails that were present in the old dialog
    - Added cancel button to match most system dialogs
    - Added note explaining what screen record does for the n00bs
    - Changed strings so that people that speak english can understand them :p
    - Added summaries to the preferences to explain options to users

--- core/java/com/android/internal/os/DeviceKeyHandler.java
+++ core/java/com/android/internal/os/DeviceKeyHandler.java
@@ -17,6 +17,7 @@
 
 package com.android.internal.os;
 
+import android.content.Intent;
 import android.view.KeyEvent;
 
 public interface DeviceKeyHandler {
@@ -28,5 +29,48 @@ public interface DeviceKeyHandler {
      * @param event The key event to be handled
      * @return null if event is consumed, KeyEvent to be handled otherwise
      */
-    public KeyEvent handleKeyEvent(KeyEvent event);
+    public boolean handleKeyEvent(KeyEvent event);
+
+    /**
+     * Invoked when an unknown key was detected by the system,
+     * this should NOT handle the key just return if it WOULD be handled
+     *
+     * @param event The key event to be handled
+     * @return If the event will be consumed
+     */
+    public boolean canHandleKeyEvent(KeyEvent event);
+
+    /**
+     * Return false if this event should be ignored
+     *
+     * @param event The key event to be handled
+     * @return If the event should be ignored
+     */
+    public boolean isDisabledKeyEvent(KeyEvent event);
+
+    /**
+     * Special key event that should be treated as
+     * a camera launch event
+     *
+     * @param event The key event to be handled
+     * @return If the event is a camera launch event
+     */
+    public boolean isCameraLaunchEvent(KeyEvent event);
+
+    /**
+     * Special key event that should be treated as
+     * a wake event
+     *
+     * @param event The key event to be handled
+     * @return If the event is a wake event
+     */
+    public boolean isWakeEvent(KeyEvent event);
+
+    /**
+     * Return an Intent that should be launched for that KeyEvent
+     *
+     * @param event The key event to be handled
+     * @return an Intent or null
+     */
+    public Intent isActivityLaunchEvent(KeyEvent event);
 }
