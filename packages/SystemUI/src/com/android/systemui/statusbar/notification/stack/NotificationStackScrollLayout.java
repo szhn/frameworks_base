d595c8bfdc59c7d8dc69d4e4cfec01011c87d7af
00day0 <therandomuser11@gmail.com>
2020-04-12 02:10:22 +0200

    Add ripple effect to oos dismiss all

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -708,7 +708,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
         if (needsColorRefresh) {
             mBackgroundColor = mContext.getColor(R.color.recents_dismiss_all_background_color);
             mIconColor = mContext.getColor(R.color.recents_dismiss_all_icon_color);
-            StatusBar.updateDismissAllButton(mBackgroundColor, mIconColor);
+            StatusBar.updateDismissAllButton(mIconColor);
             needsColorRefresh = false;
         }
     }
@@ -818,7 +818,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
         updateBackgroundDimming();
         mShelf.onUiModeChanged();
         mSectionsManager.onUiModeChanged();
-        StatusBar.updateDismissAllButton(mBackgroundColor, mIconColor);
+        StatusBar.updateDismissAllButton(mIconColor);
     }
 
     @ShadeViewRefactor(RefactorComponent.DECORATOR)
